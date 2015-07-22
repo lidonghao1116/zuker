d4db0f3ee4bacd56044afa1c13f5fb20a30b1870
hungmi <hungmihungmi@gmail.com>
2015-07-22 12:03:34 +0800

    TODO:   同樣電話號碼不可重複註冊 done   電話號碼格式驗證
    Test:
      確認完密碼是否回傳正確的使用者
      同樣電話號碼不可重複註冊
      電話號碼格式驗證

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :name, presence: true
-  validates :phone_number, presence: true
   validates :password, length: { in: 6..20 }, confirmation: true
+  
+  validates :name, presence: true
+  validates :phone_number, presence: true, uniqueness: true
+  #validates :email, uniqueness: true, :allow_blank => true
 end
